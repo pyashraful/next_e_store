a95a8983067648eac438fb1287a0900a58ce02e3
Py-Ashraful <ashrafulislam180@gmail.com>
2022-02-16 22:39:59 +0600

    card design

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import Layout from "../components/Layout";
+import Card from "../components/Card";
 
 export default function Home() {
   return (
     <Layout>
-      <h1 className="text-3xl text-center bg-blend-color-dodge text-blue-600 font-bold italic">
-        Hello world!
-      </h1>
+      <Card />
     </Layout>
   );
 }
