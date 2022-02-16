41b75c43b1833b4f4d0c45fb14e672a67bad9c9b
Py-Ashraful <ashrafulislam180@gmail.com>
2022-02-16 13:44:47 +0600

    navbar height

--- components/Layout.js
+++ components/Layout.js
@@ -2,16 +2,19 @@ import Head from "next/head";
 import Search from "./Seach";
 import CardBadge from "./CardBadge";
 import ProfileIcon from "./PorfileIcon";
+import Logo from "../public/Logo.svg";
 import { Typography, Container } from "@mui/material";
 import { Box } from "@mui/system";
 
 const boxStyle = {
   display: "flex",
   justifyContent: "space-between",
+  alignItems: "center",
   maxWidth: "1200px",
   margin: "0 auto",
   padding: "10px 20px",
   backgroundColor: "#c39e6b",
+  height: "80px",
 };
 
 export default function Layout({ children }) {
@@ -21,7 +24,8 @@ export default function Layout({ children }) {
         <title>My page</title>
       </Head>
       <Box sx={boxStyle}>
-        <Typography>My page</Typography>
+        {/* <Typography>My page</Typography> */}
+        <Logo width="99px" height="28px" />
         <Search />
         <div>
           <CardBadge />
