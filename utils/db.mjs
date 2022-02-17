648a3730dcf14c70fb4fcf2a79d5c0254f194717
Py-Ashraful <ashrafulislam180@gmail.com>
2022-02-17 23:14:19 +0600

    remove mongoose setting

--- utils/db.mjs
+++ utils/db.mjs
@@ -18,11 +18,7 @@ async function connect() {
     await mongoose.disconnect();
   }
 
-  const db = await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  });
+  const db = await mongoose.connect(process.env.MONGO_URI);
 
   console.log("new connection");
   connection.isConnected = db.connections[0].readyState;
