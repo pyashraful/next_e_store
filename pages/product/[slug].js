602ed450e12c9eb6274209cbe9b16c2f7847d4dc
Py-Ashraful <ashrafulislam180@gmail.com>
2022-04-04 23:46:41 +0600

    details page

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,8 +1,37 @@
+import Image from "next/image";
 import connectDB from "../../utils/db.mjs";
 import ProductModel from "../../model/productSchema";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import { Container } from "@mui/material";
 
 export default function Details({ product }) {
-  return <h1>Details</h1>;
+  console.log("🚀 ~ file: [slug].js ~ line 5 ~ Details ~ product", product);
+
+  return (
+    <Container>
+      <Grid container>
+        <Grid item xs={6} sx={{ width: "100%" }}>
+          <Box>
+            <Box sx={{ display: "flex", justifyContent: "center" }}>
+              <Image
+                className="p-8 rounded-t-lg"
+                src={`${product.image}`}
+                alt="product image"
+                width={300}
+                height={300}
+                layout="responsive"
+              />
+            </Box>
+            <Box></Box>
+          </Box>
+        </Grid>
+        <Grid item xs={6}>
+          helloworld
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export async function getServerSideProps(context) {
