e4a8e1e9375b49233fae20b2a095347c5e8b42d9
Py-Ashraful <ashrafulislam180@gmail.com>
2022-04-04 23:13:41 +0600

    getServerSideProps for product details

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,9 +1,35 @@
-import { useRouter } from "next/router";
+import connectDB from "../../utils/db.mjs";
+import ProductModel from "../../model/productSchema";
 
-export default function Details() {
-  const route = useRouter();
-  const { id } = route.query;
-  console.log(id);
-
+export default function Details({ product }) {
   return <h1>Details</h1>;
+}
+
+export async function getServerSideProps(context) {
+  console.log(context);
+  const { params } = context;
+  console.log(
+    "🚀 ~ file: [slug].js ~ line 14 ~ getServerSideProps ~ params",
+    params
+  );
+  const { slug } = params;
+  console.log(
+    "🚀 ~ file: [slug].js ~ line 16 ~ getServerSideProps ~ slug",
+    slug
+  );
+
+  await connectDB();
+  const data = await ProductModel.findOne({ slug }).lean();
+  const product = JSON.parse(JSON.stringify(data));
+
+  // console.log(
+  //   "🚀 ~ file: index.js ~ line 19 ~ getServerSideProps ~ products",
+  //   products
+  // );
+
+  return {
+    props: {
+      product,
+    },
+  };
 }
