328f35d2c9303a5eed492654055040c47042ec8b
Py-Ashraful <ashrafulislam180@gmail.com>
2022-04-08 17:31:23 +0600

    search button

--- components/Search.js
+++ components/Search.js
@@ -3,6 +3,8 @@ import SearchIcon from "@mui/icons-material/Search";
 import { styled } from "@mui/system";
 const MyTextfild = styled(OutlinedInput)({
   borderRadius: "40px",
+  overflow: "hidden",
+  paddingRight: 0,
   "& .MuiOutlinedInput-input": {
     paddingTop: "10px",
     paddingBottom: "10px",
@@ -14,10 +16,18 @@ export default function Search() {
     <Box sx={{ maxWidth: 670, mx: "auto", flex: "1 1 0" }}>
       <FormControl sx={{ width: "100%" }}>
         <MyTextfild
-          sx={{}}
+          sx={{ overflow: "hidden" }}
           placeholder="Searching for..."
           startAdornment={<SearchIcon />}
-          endAdornment={<Button>Search</Button>}
+          endAdornment={
+            <Button
+              variant="contained"
+              size="large"
+              sx={{ height: 44, width: 140 }}
+            >
+              Search
+            </Button>
+          }
         />
       </FormControl>
     </Box>
