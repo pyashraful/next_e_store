77231cdac23fb2bee61830db41548b0fda2dfa1f
Py-Ashraful <ashrafulislam180@gmail.com>
2022-04-17 14:37:07 +0600

    user model

---
+++ model/user.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: { type: String, unique: true, required: true, trim: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+export default User;
