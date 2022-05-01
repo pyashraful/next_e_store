ed1b674baa2a5e516dd08370624b5de261c88805
Py-Ashraful <ashrafulislam180@gmail.com>
2022-05-01 23:54:07 +0600

    profile push to home

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,7 +1,19 @@
+import { useEffect, useContext } from "react";
 import UserLayout from "../../components/profile/UserLayout";
 import ProfileMain from "../../components/profile/ProfileMain";
+import { Store } from "../../utils/store";
+import { useRouter } from "next/router";
 
 export default function Profile() {
+  const router = useRouter();
+  const [state] = useContext(Store);
+
+  useEffect(() => {
+    if (Object.keys(state.userInfo).length === 0) {
+      router.push("/");
+    }
+  }, [router, state.userInfo]);
+
   return (
     <UserLayout>
       <ProfileMain />
