d095e440e1b305ab599f197afe779dd2eb7c3419
Py-Ashraful <ashrafulislam180@gmail.com>
2022-05-05 23:40:53 +0600

    styled components bug fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   reactStrictMode: true,
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
