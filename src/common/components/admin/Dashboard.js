c560cb76ca98527f5b85a20da67798dc752afa15
Py-Ashraful <ashrafulislam180@gmail.com>
2022-05-09 23:09:21 +0600

    Dashbord Balance

--- src/common/components/admin/Dashboard.js
+++ src/common/components/admin/Dashboard.js
@@ -1,10 +1,116 @@
+import { Grid, Paper } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
 import AdminHeader from "./AdminHeader";
 
 export default function DashBoard() {
   return (
-    <div>
+    <>
       <AdminHeader />
-    </div>
+      <Grid container spacing={3}>
+        <Grid item xs={12} lg={4}>
+          <Paper sx={{ pt: 3, pb: 3 }}>
+            <Box
+              component="h5"
+              sx={{
+                m: 0,
+                mb: 1,
+                fontSize: 16,
+                fontWeight: 600,
+                textAlign: "center",
+              }}
+            >
+              Earnings (before taxes)
+            </Box>
+            <Box
+              component="h1"
+              sx={{
+                m: 0,
+                mb: 0.5,
+                fontSize: 30,
+                fontWeight: 700,
+                textAlign: "center",
+              }}
+            >
+              $30450.00
+            </Box>
+            <Box
+              component="p"
+              sx={{ m: 0, fontSize: 16, fontWeight: 600, textAlign: "center" }}
+            >
+              Earnings (before taxes)
+            </Box>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} lg={4}>
+          <Paper sx={{ pt: 3, pb: 3 }}>
+            <Box
+              component="h5"
+              sx={{
+                m: 0,
+                mb: 1,
+                fontSize: 16,
+                fontWeight: 600,
+                textAlign: "center",
+              }}
+            >
+              Earnings (before taxes)
+            </Box>
+            <Box
+              component="h1"
+              sx={{
+                m: 0,
+                mb: 0.5,
+                fontSize: 30,
+                fontWeight: 700,
+                textAlign: "center",
+              }}
+            >
+              $30450.00
+            </Box>
+            <Box
+              component="p"
+              sx={{ m: 0, fontSize: 16, fontWeight: 600, textAlign: "center" }}
+            >
+              Earnings (before taxes)
+            </Box>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} lg={4}>
+          <Paper sx={{ pt: 3, pb: 3 }}>
+            <Box
+              component="h5"
+              sx={{
+                m: 0,
+                mb: 1,
+                fontSize: 16,
+                fontWeight: 600,
+                textAlign: "center",
+              }}
+            >
+              Earnings (before taxes)
+            </Box>
+            <Box
+              component="h1"
+              sx={{
+                m: 0,
+                mb: 0.5,
+                fontSize: 30,
+                fontWeight: 700,
+                textAlign: "center",
+              }}
+            >
+              $30450.00
+            </Box>
+            <Box
+              component="p"
+              sx={{ m: 0, fontSize: 16, fontWeight: 600, textAlign: "center" }}
+            >
+              Earnings (before taxes)
+            </Box>
+          </Paper>
+        </Grid>
+      </Grid>
+    </>
   );
 }
