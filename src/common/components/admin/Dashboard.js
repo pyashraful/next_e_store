3519943ce2b145a8e4e7c35f27025cf547e06372
Py-Ashraful <ashrafulislam180@gmail.com>
2022-05-09 23:29:53 +0600

    dashboard selce

--- src/common/components/admin/Dashboard.js
+++ src/common/components/admin/Dashboard.js
@@ -1,5 +1,5 @@
 import { Grid, Paper } from "@mui/material";
-import { Box } from "@mui/system";
+import { Box, fontSize } from "@mui/system";
 import React from "react";
 import AdminHeader from "./AdminHeader";
 
@@ -107,6 +107,14 @@ export default function DashBoard() {
               sx={{ m: 0, fontSize: 16, fontWeight: 600, textAlign: "center" }}
             >
               Earnings (before taxes)
+            </Box>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12} lg={8}>
+          <Paper sx={{ px: 3.75, py: 2.5 }}>
+            <Box component={"h5"} sx={{ mb: 3, mt: 0, fontSize: 16 }}>
+              Seles
             </Box>
           </Paper>
         </Grid>
