fa3d3bc202db0c0eec28c998ca6c49213d30e920
Py-Ashraful <ashrafulislam180@gmail.com>
2022-05-11 23:33:26 +0600

    product item paper style fixed

--- src/common/components/product/ProductsItems.jsx
+++ src/common/components/product/ProductsItems.jsx
@@ -22,59 +22,61 @@ export default function ProductsItems({ products }) {
       >
         {products.map((product) => (
           <Grid sx={{ flex: "1 1 0" }} item lg={3} xl={3} key={product._id}>
-            <Paper>
-              <Box>
-                <Link href={`/product/${product._id}`} passHref>
-                  <a>
-                    <Image
-                      src={`${product.image}`}
-                      alt="product image"
-                      width={554}
-                      height={540}
-                      layout="responsive"
-                    />
-                  </a>
-                </Link>
-              </Box>
-              <Box sx={{ p: 2 }}>
-                <Stack direction="row" justifyContent="space-between">
-                  <Box>
-                    <Box>
-                      <Link href={`/product/${product._id}`} passHref>
-                        <Typography
-                          sx={{
-                            fontWeight: 600,
-                            overflow: "hidden",
-                            textOverflow: "ellipsis",
-                          }}
-                          variant="subtitle1"
-                          component="h3"
-                          noWrap={true}
-                        >
-                          {product.title}
-                        </Typography>
-                      </Link>
-                      <Rating
-                        name="product rating"
-                        precision={0.5}
-                        value={product.rating}
-                        readOnly
+            <Box sx={{ py: 0.5 }}>
+              <Paper sx={{ overflow: "hidden" }}>
+                <Box>
+                  <Link href={`/product/${product._id}`} passHref>
+                    <a>
+                      <Image
+                        src={`${product.image}`}
+                        alt="product image"
+                        width={554}
+                        height={540}
+                        layout="responsive"
                       />
-                    </Box>
+                    </a>
+                  </Link>
+                </Box>
+                <Box sx={{ p: 2 }}>
+                  <Stack direction="row" justifyContent="space-between">
                     <Box>
-                      <Box sx={{ fontWeight: 600, color: "primary.main" }}>
-                        ${product.price}
+                      <Box>
+                        <Link href={`/product/${product._id}`} passHref>
+                          <Typography
+                            sx={{
+                              fontWeight: 600,
+                              overflow: "hidden",
+                              textOverflow: "ellipsis",
+                            }}
+                            variant="subtitle1"
+                            component="h3"
+                            noWrap={true}
+                          >
+                            {product.title}
+                          </Typography>
+                        </Link>
+                        <Rating
+                          name="product rating"
+                          precision={0.5}
+                          value={product.rating}
+                          readOnly
+                        />
+                      </Box>
+                      <Box>
+                        <Box sx={{ fontWeight: 600, color: "primary.main" }}>
+                          ${product.price}
+                        </Box>
                       </Box>
                     </Box>
-                  </Box>
-                  <CartButton
-                    iconsize="outlineSquare"
-                    item={product}
-                    flow="column-reverse"
-                  />
-                </Stack>
-              </Box>
-            </Paper>
+                    <CartButton
+                      iconsize="outlineSquare"
+                      item={product}
+                      flow="column-reverse"
+                    />
+                  </Stack>
+                </Box>
+              </Paper>
+            </Box>
           </Grid>
         ))}
       </Grid>
