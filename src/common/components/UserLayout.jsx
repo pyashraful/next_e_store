cc3537791d089b9e67f2142294ea45a1b8accb87
Py-Ashraful <ashrafulislam180@gmail.com>
2022-05-12 21:21:00 +0600

    userLayout

--- src/common/components/UserLayout.jsx
+++ src/common/components/UserLayout.jsx
@@ -1,18 +1,17 @@
 import { Container, Grid } from "@mui/material";
 import Layout from "./Layout";
+import ProfileSidebar from "@components/profile/ProfileSidebar";
 
-export default function UserLayout({ sideber, main }) {
+export default function UserLayout({ children }) {
   return (
     <Layout>
       <Container>
         <Grid container spacing={3}>
           <Grid item xs={12} lg={3}>
-            {sideber}
-            {/* <ProfileSidebar /> */}
+            <ProfileSidebar />
           </Grid>
           <Grid item xs={12} lg={9}>
-            {main}
-            {/* {children} */}
+            {children}
           </Grid>
         </Grid>
       </Container>
--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,6 +1,5 @@
 import { useEffect, useContext } from "react";
 import UserLayout from "@components/UserLayout";
-import ProfileSidebar from "@components/profile/ProfileSidebar";
 import ProfileMain from "@components/profile/ProfileMain";
 import { Store } from "@utils/store";
 import { useRouter } from "next/router";
@@ -10,14 +9,6 @@ export default function Profile() {
   const { user, isLoading } = useUser();
   const router = useRouter();
   const { state } = useContext(Store);
-  if (!user) {
-    if (isLoading) {
-      return <h1>Loading</h1>;
-    } else {
-      router.push("/");
-    }
-  } else {
-  }
 
   // useEffect(() => {
   //   if (!state.userInfo) {
@@ -25,5 +16,9 @@ export default function Profile() {
   //   }
   // }, [router, state.userInfo]);
 
-  return <UserLayout sideber={<ProfileSidebar />} main={<ProfileMain />} />;
+  return (
+    <UserLayout>
+      <ProfileMain />
+    </UserLayout>
+  );
 }
