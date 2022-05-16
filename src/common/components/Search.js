f87e75f4d857877ea271b5d96d48b36d3b2dd3cd
Py-Ashraful <ashrafulislam180@gmail.com>
2022-05-16 23:54:19 +0600

    handleSearch

--- src/common/components/Search.js
+++ src/common/components/Search.js
@@ -1,4 +1,5 @@
-import { Box, Button, FormControl, OutlinedInput } from "@mui/material";
+import { useState } from "react";
+import { Box, Button, FormControl, OutlinedInput, Paper } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import { styled } from "@mui/system";
 const MyTextfild = styled(OutlinedInput)({
@@ -11,22 +12,30 @@ const MyTextfild = styled(OutlinedInput)({
   },
 });
 
-const SerarchResult = styled(Box)((theme) => ({
+const SerarchResult = styled(Paper)((theme) => ({
   position: "absolute",
   top: "100%",
-  left: 22,
+  left: 0,
   height: 250,
-  background: "#ddd",
-  maxWidth: "94%",
+  minWidth: "100%",
 }));
 
 export default function Search() {
+  const [searchResult, setSearchResult] = useState("");
+
+  const handleSearch = (e) => {
+    setSearchResult(e.target.value);
+  };
+
   return (
     <Box
       sx={{ maxWidth: 670, mx: "auto", flex: "1 1 0", position: "relative" }}
     >
       <FormControl sx={{ width: "100%" }}>
         <MyTextfild
+          onChange={(e) => {
+            handleSearch(e);
+          }}
           sx={{ overflow: "hidden" }}
           placeholder="Searching for..."
           startAdornment={<SearchIcon />}
@@ -50,7 +59,7 @@ export default function Search() {
           }
         />
       </FormControl>
-      <SerarchResult></SerarchResult>
+      {searchResult.length > 0 && <SerarchResult></SerarchResult>}
     </Box>
   );
 }
