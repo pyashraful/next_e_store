d84bdf9d8a69d97c3c01de9a133e4d9b3f495e19
Py-Ashraful <ashrafulislam180@gmail.com>
2022-05-22 23:52:25 +0600

    Order api bug

--- src/model/order.js
+++ src/model/order.js
@@ -1,64 +1,69 @@
 import mongoose from "mongoose";
 
-const order = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  orderItems: [
-    {
-      productId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
-        required: true,
+const order = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    orderItems: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        name: { type: String, required: true },
+        price: { type: Number, required: true },
+        image: { type: String, required: true },
+        quantity: { type: Number, required: true },
       },
+    ],
+    shippingAddress: {
       name: { type: String, required: true },
-      price: { type: Number, required: true },
-      image: { type: String, required: true },
-      quantity: { type: Number, required: true },
+      email: { type: String, required: true },
+      phone: { type: String, required: true },
+      zipcode: { type: String, required: true },
+      country: { type: String, required: true },
+      address: { type: String, required: true },
     },
-  ],
-  shippingAddress: {
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    phone: { type: String, required: true },
-    zipcode: { type: String, required: true },
-    country: { type: String, required: true },
-    address: { type: String, required: true },
-  },
 
-  paymentMethod: {
-    type: String,
-    required: true,
-  },
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
 
-  totalPrice: {
-    type: Number,
-    required: true,
-    default: 0,
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    taxPrice: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    shippingCost: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    discount: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+    status: {
+      type: String,
+      default: "pending",
+      enum: ["pending", "processing", "delivered", "canceled"],
+    },
+    createdAt: {
+      type: Date,
+      required: true,
+    },
   },
-  taxPrice: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-  shippingCost: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-  discount: {
-    type: Number,
-    default: 0,
-    required: true,
-  },
-  status: {
-    type: String,
-    default: "pending",
-    enum: ["pending", "processing", "delivered", "canceled"],
-  },
-  createdAt: {
-    type: Date,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Order = mongoose.models.User || mongoose.model("Order", order);
 export default Order;
--- src/pages/api/order.js
+++ src/pages/api/order.js
@@ -1,10 +1,14 @@
 import Order from "@model/order";
+import User from "@model/user";
 export default async (req, res) => {
   let data = req.body;
   console.log("🚀 ~ file: order.js ~ line 5 ~ order", data);
 
   try {
-    const order = await new Order({ ...data });
+    const user = await User.findById({ _id: data.user });
+    const newOrder = await new Order({ ...data, user: user._id });
+    const order = await newOrder.save();
+    console.log("🚀 ~ file: order.js ~ line 9 ~ order2", newOrder);
     console.log("🚀 ~ file: order.js ~ line 8 ~ order", order);
     res.json({ order });
   } catch (err) {
